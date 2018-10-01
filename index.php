783d7620223c656aa9b312ed2ae973d31876e6b8
manohar <manoharsah50@gmail.com>
2018-10-02 08:32:05 +1000

    text change

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $_SESSION['calculation_id'] = false;
 			<div class="col-md-6 col-md-offset-3">
 				<div class="well well-sm">
 				  <h3>What is Staff Turn Calculator?</h3>
-				  <p>For example budgeting for technician Holidays (they are away for 4 weeks, so this is the impact to my monthly target in that time)  - Used to Justify pay rise request to business owner (if they leave it costs us this much vs $xx for the pay rise)  - Used to identify the cost of sacking the technician vs the cost of their poor work/return work (ie If we sack them it might cost us $xx, but to keep them on with the mistakes they make costds us $YY)  <a href="" class="green_link" data-toggle="modal" data-target="#myModal" style="font-weight: bold;">View Examples.</a></p>
+				  <p>The primary goal of this calculator is to provide an insight into what staff turn actually cost to automotive business. For example budgeting for technician Holidays (they are away for 4 weeks, so this is the impact to my monthly target in that time)  - Used to Justify pay rise request to business owner (if they leave it costs us this much vs $xx for the pay rise)  - Used to identify the cost of sacking the technician vs the cost of their poor work/return work (ie If we sack them it might cost us $xx, but to keep them on with the mistakes they make costds us $YY)  <a href="" class="green_link" data-toggle="modal" data-target="#myModal" style="font-weight: bold;">View Examples.</a></p>
 				</div>
 			</div>
 			<div class="col-md-6 col-md-offset-3">
